8ddb5f382886f89dd57ba11e83dbc4af35178d07
liheng0817 <40493168+liheng0817@users.noreply.github.com>
2018-08-18 20:38:50 +0800

    Update auto_install_vsftpd.sh

--- auto_install_vsftpd.sh
+++ auto_install_vsftpd.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# auth:fengdeyingzi
-# func:vsftpd安装
+#describe:This script is used to install vsftpd.
+#author:H.LI
+#time:20180818
 
 export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 confdir=/etc/vsftpd
@@ -9,16 +10,40 @@ confdir=/etc/vsftpd
 ip=`curl -s  http://members.3322.org/dyndns/getip`
 
 #查看系统版本#
+sys_version () {
 sys=`rpm -q centos-release|cut -d- -f3`
 echo "Your system version is ${sys}.x"
+}
+
+create_ftpuser () {
 echo "创建FTP用户"
 read -p "Please input FTP user:" FTPUSER
 read -p "Please input FTP user password:" PASS
 useradd -m -d /home/$FTPUSER -s /sbin/nologin $FTPUSER
 echo $PASS | passwd --stdin $FTPUSER
-
+}
+#检测#
+check_vsftpd () {
+    rpm -q vsftpd
+    if [ $? -eq 0 ];then
+    	read -p "Vsftpd has been installed，are you want to remove vsftpd!，pleases input [y/n]:" PANDUAN
+    	case $PANDUAN in
+    	    y|Y/yes)
+	    yum -y remove vsftpd && rm -fr /etc/vsftpd
+	;;
+	n|N|no)
+	echo "Your choice is cancel remove"
+	exit 0
+	;;
+	*)
+	echo "You must input y|Y|yes|n|N|no!"
+	exit 1
+	;;
+	esac
+       fi
+}
 #安装vsftpd函数
-install_vsftpd () {
+init_vsftpd () {
 yum install -y vsftpd
 cd $confdir
 cat >vsftpd.conf <<EOF
@@ -41,18 +66,27 @@ pasv_max_port=2050
 EOF
 }
 #安装配置vsftpd#
+install_vsftpd () {
 echo "安装配置vsftpd"
 if [ $sys -eq 6 ];
    then
-	install_vsftpd
+	init_vsftpd
 	/etc/init.d/vsftpd start
 elif [ $sys -eq 7 ];
    then
-	install_vsftpd
+	init_vsftpd
 	systemctl start vsftpd
 else	
+	echo "This script only applies to version 7 and 6"
 	exit 1
 fi
-
+}
+main () {
+sys_version
+check_vsftpd
+install_vsftpd
+create_user
+}
+main
 echo -e "\033[42;37m Vsftpd is Install Successed,server-ip:$ip ftpuser:$FTPUSER Password:$PASS \033[0m"
 echo -e "\033[42;37m 如您开启了系统防火墙或者安全组，请关闭系统防火墙或者配置系统防火墙21及2000到2050端口的放行规则，并在安全组中设置21端口及2000到2050端口放行规则 \033[0m"
